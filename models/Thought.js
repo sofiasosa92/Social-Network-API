13523a2364d9c6a46aa21efc503b710c2514facd
Sofia92 <sofiasosaperez@outlook.com>
2023-03-14 22:00:55 -0500

    done with Thought file

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const {Schema, model, Types} = require('mongoose');
+//moment is a library that allows to format dates
+const moment = require('moment');
+
+// Reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+// Thought schema
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+}   
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
