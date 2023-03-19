693defd1f898e0848e9b70340f3fd6fc0184bd1c
Sofia92 <sofiasosaperez@outlook.com>
2023-03-19 18:56:52 -0500

    fixing get thought error

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,15 +10,19 @@ const {
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
+//http://localhost:3001/api/thoughts/
 router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:id
+//http://localhost:3001/api/thoughts/1
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+//http://localhost:3001/api/thoughts/1/reactions
 router.route('/:thoughtId/reactions').post(createReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
+//http://localhost:3001/api/thoughts/1/reactions/2
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
